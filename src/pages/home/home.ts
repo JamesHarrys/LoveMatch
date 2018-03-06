2f1ef7246d03c3dd0d2f07d4473195e4b0dcccc4
JamesHarrys <bissickj@gmail.com>
2018-03-06 10:20:57 +0100

    Finished

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,6 +7,16 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  nameA = '';
+  nameB = '';
+  get score() {
+    const letters = (this.nameA + this.nameB).toLowerCase();
+    let sum = 0;
+    for (let i = 0; i < letters.length; i++) {
+      sum += letters.charCodeAt(i);
+    }
+    return sum % 101;
+  }
   constructor(public navCtrl: NavController) {
 
   }
